18d44769f48caee7784e0b73a099df8bed70f6ce
nerdy-adams <nerdrun.adams@gmail.com>
2018-01-29 13:21:29 +0900

    #2 커스터마이징 - nav, home, img change

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+let jsonParser = bodyParser.json()
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use('/public', express.static(__dirname + '/public'));
+app.set('views', __dirname+'/views')
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.get('/', (req,res) => {
+  res.render('index.html')
+})
+app.get('/single', (req, res) => {
+  res.render('single.html')
+})
+app.listen(3000, () => {
+  console.log('hello');
+})
